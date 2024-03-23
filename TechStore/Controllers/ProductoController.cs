9db925cd7c26853a98f66e05c1d4bd7843d74d3d
YanpieroCutipa <yanpiero_cutipa@usmp.pe>
2024-03-23 10:14:06 -0500

    Formulario Agregar Producto

---
+++ TechStore/Controllers/ProductoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TechStore.Models;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TechStore.Controllers
+{
+    public class ProductoController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create([Bind("Nombre,Descripcion,Precio")] Producto producto)
+        {
+            if (ModelState.IsValid)
+            {
+
+                ViewData["Message"] = "Registro correcto! " +
+                    "\nNombre: " + producto.Nombre +
+                    "\nDescripcion: " + producto.Descripcion +
+                     "\nPrecio: " + producto.Precio +
+                    "\nIGV (18%): " + producto.IGV();
+
+                return View("Index");
+            }
+
+            return View("Index");
+        }
+    }
+}
---
+++ TechStore/Models/Producto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TechStore.Models
+{
+    public class Producto
+    {
+        [Display(Name = "Nombre del Producto")]
+        [Required(ErrorMessage = "El campo del nombre producto es obligatorio.")]
+        public string Nombre { get; set; }
+
+        [Display(Name = "Descripcion del Producto")]
+        [Required(ErrorMessage = "El campo de la descripcion es obligatorio.")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Precio del producto")]
+        [Required(ErrorMessage = "El campo del precio es obligatorio.")]
+        public double Precio { get; set; }
+
+        public double IGV()
+        {
+            return Precio * 0.18;
+        }
+    }
+}
